b5284b1811653147dd1490f3bab19b58f0f6fc33
MasayaArao <135002352+MasayaArao@users.noreply.github.com>
2023-05-30 16:31:48 +0900

    Delete sekisanapp _fld2 directory

--- sekisanapp _fld2/snippets/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from snippets.models import Snippet
-from .models import FileUpload
-
-admin.site.register(Snippet)
-admin.site.register(FileUpload)
--- sekisanapp _fld2/test_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'test_app'
-
-urlpatterns = [
-        path('', views.index, name='index'),
-        path('new_file', views.new_file, name='new_file'),  # 追加
-]
